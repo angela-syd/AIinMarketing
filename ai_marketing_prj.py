21ecfc38b53f868eaa6e119cd3e9f23ccd465ab4
angela-syd <132868762+angela-syd@users.noreply.github.com>
2024-05-11 06:29:58 +0400

    Update ai_marketing_prj.py

--- ai_marketing_prj.py
+++ ai_marketing_prj.py
@@ -1,5 +1,3 @@
-
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -44,6 +42,20 @@ with zipfile.ZipFile(zip_file_path, 'r') as z:
     with z.open(csv_file_name) as csv_file:
         # Read the CSV file into a DataFrame
         dftemp = pd.read_csv(csv_file)
+        
+
+# List of columns to drop
+columns_to_drop = ['reviewerName', 'rank', 'date', 'fit', 'reviewerID', 'verified', 'unixReviewTime', 'feature']
+
+# Drop the specified columns
+dftemp = dftemp.drop(columns=columns_to_drop)
+# Define the columns to check for missing values
+columns_to_check = ['reviewText', 'summary', 'title']
+# Drop rows with missing values in the specified columns
+dftemp = dftemp.dropna(subset=columns_to_check)
+# Replace missing values in the 'brand' column with "Others"
+dftemp['brand'] = dftemp['brand'].fillna("Others")
+    
 
 
 # Set up logging
@@ -53,99 +65,244 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 # Load datasets
 @st.cache_data
 def load_data():
-    df1 = pd.read_csv(r"sentimentdata.csv")
-    df2 = dftemp.copy()
-    return df1, df2
+    df11 = pd.read_csv(r"sentimentdata.csv")
+    df22 = dftemp.copy()
+    return df11, df22
 
-df1, df2 = load_data()
+df11, df22 = load_data()
 
-# Function to preprocess text and perform sentiment analysis
-def preprocess_and_analyze(df):
-    df['cleanedText'] = df['reviewText'].apply(lambda text: re.sub('https?://\S+|www\.\S+', '', str(text).lower()))
-    df['cleanedText'] = df['cleanedText'].apply(lambda text: re.sub('[%s]' % re.escape(string.punctuation), '', text))
-    df['sentiment_score'] = df['cleanedText'].apply(lambda x: TextBlob(x).sentiment.polarity)
-    df['sentiment_type'] = df['sentiment_score'].apply(lambda x: 'positive' if x > 0 else ('neutral' if x == 0 else 'negative'))
+#ReviewTime Column
+# Splitting the 'reviewTime' column
+date_split = df22["reviewTime"].str.split(", ", n=1, expand=True)
 
-preprocess_and_analyze(df1)
+# Splitting the date further into 'month' and 'day'
+month_day_split = date_split[0].str.split(" ", n=1, expand=True)
 
-# Sidebar configuration
-st.sidebar.header("Configuration")
-dataset_choice = st.sidebar.selectbox("Choose the dataset for analysis:", ["Processed Data", "Raw Data"], index=0)
-#analysis_type = st.sidebar.radio("Select analysis type:", ["Sentiment Analysis", "Data Overview"])
+# Adding 'year', 'month', and 'day' to the main dataset
+df22["year"] = date_split[1]
+df22["month"] = month_day_split[0]  # Extracting the month
+df22["day"] = month_day_split[1]    # Extracting the day
 
-# Conditionally display the analysis type radio button
-if dataset_choice == "Processed Data":
-    analysis_type = st.sidebar.radio("Select analysis type:", ["Sentiment Analysis", "Data Overview"])
+# Dropping the original 'reviewTime' column
+df22.drop(columns=["reviewTime"], inplace=True)
 
-    if analysis_type == "Sentiment Analysis":
+# Dropdown menus for brand, year, and month in a single row
+col1, col2, col3 = st.columns(3)
+with col1:
+    brands = ['All'] + list(df11['brand'].unique())
+    selected_brand = st.selectbox('Select Brand:', brands)
+with col2:
+    years = ['All'] + list(df11['year'].unique())
+    selected_year = st.selectbox('Select Year:', years)
+with col3:
+    months = ['All'] + list(df11['month'].unique())
+    selected_month = st.selectbox('Select Month:', months)
 
-        st.write("Sentiment Analysis Details here...")   #figure out how to make it dynamic
+# Applying filters to the data
+# Filter by brand
+if selected_brand != 'All':
+    df1 = df11[df11['brand'] == selected_brand]
+    df2 = df22[df22['brand'] == selected_brand]
+else:
+    df1 = df11.copy()
+    df2 = df22.copy()
+
+# Filter by year
+if selected_year != 'All':
+    df1 = df1[df1['year'] == selected_year]
+    df2 = df2[df2['year'] == selected_year]
+
+# Filter by month
+if selected_month != 'All':
+    df1 = df1[df1['month'] == selected_month]
+    df2 = df2[df2['month'] == selected_month]
+    
+    
+########Data Preprocessing
+
+#change to round off instead
+df2['overall'] = df2['overall'].apply(round)
+
+#Figuring out the distribution of categories
+overall_counts = df2['overall'].value_counts()
+
+# Define a function to map ratings to sentiment categories
+def map_sentiment(rating):
+    if rating >= 4:
+        return 'positive'
+    elif rating == 3:
+        return 'neutral'
+    else:
+        return 'negative'
+
+# Create the sentiment column by applying the function to the 'overall' column
+df2['sentiment'] = df2['overall'].apply(map_sentiment)
 
 
-
-if dataset_choice == "Processed Data":
-    if analysis_type == "Sentiment Analysis":
-        positive_reviews = df1[df1['sentiment_type'] == 'positive']['cleanedText']
-        if not positive_reviews.empty:
-            wordcloud_text = ' '.join(positive_reviews)
-            wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white').generate(wordcloud_text)
-            fig, ax = plt.subplots()
-            plt.imshow(wordcloud, interpolation='bilinear')
-            plt.axis("off")
-            st.pyplot(fig)
-        else:
-            st.write("No positive reviews available for generating a word cloud.")
-
-# Function to preprocess text
-def preprocess_text(text):
-    text = str(text).lower() 
+####Sentiment Analysis
+def review_cleaning(text):
+    '''Make text lowercase, remove text in square brackets,remove links,remove punctuation
+    and remove words containing numbers.'''
+    text = str(text).lower()
     text = re.sub('\[.*?\]', '', text)
     text = re.sub('https?://\S+|www\.\S+', '', text)
     text = re.sub('<.*?>+', '', text)
     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
     text = re.sub('\n', '', text)
     text = re.sub('\w*\d\w*', '', text)
-    stop_words = set(stopwords.words('english'))
-    text = ' '.join([word for word in text.split() if word not in stop_words])
     return text
 
+# Clean the reviewText column
+df2['reviewText'] = df2['reviewText'].apply(review_cleaning)
 
-# Display raw data selection
-def display_data(data):
-    st.dataframe(data.head())
+import nltk
+from nltk.stem import PorterStemmer
+nltk.download('stopwords')
+
+# Define stopwords
+stop_words = set(stopwords.words('english'))
+# Function to remove stopwords
+def remove_stopwords(text):
+    """Custom function to remove the stopwords"""
+    return " ".join([word for word in str(text).split() if word not in STOPWORDS])
+
+# Apply stopword removal and overwrite reviewText_preprocessed column
+df2["reviewText"] = df2["reviewText"].apply(lambda text: remove_stopwords(text))
+
+#Spell Checker
+from spellchecker import SpellChecker
+spell = SpellChecker()
+def correct_spellings(text):
+    corrected_text = []
+    misspelled_words = spell.unknown(text.split())
+    for word in text.split():
+        if word in misspelled_words:
+            corrected_text.append(spell.correction(word))
+        else:
+            corrected_text.append(word)
+    return " ".join(corrected_text)
 
 
-def plot_data(df, title):
-    fig, ax = plt.subplots()
-    df['overall'].plot(kind='hist', bins=20, title=title)
-    plt.gca().spines[['top', 'right']].set_visible(False)
-    st.pyplot(fig)
+# Conducting sentiment analysis on reviewText
+df2['sentiment_score_reviewText'] = df2['reviewText'].apply(lambda x: TextBlob(str(x)).sentiment.polarity)
+df2['sentiment_reviewText'] = df2['sentiment_score_reviewText'].apply(lambda x: 'positive' if x > 0 else ('neutral' if x == 0 else 'negative'))
+
+#####end of Sentiment Analysis
+
+###Configuration
+st.sidebar.header("Configuration")
+dataset_choice = st.sidebar.selectbox("Choose the dataset for analysis:", ["Processed Data", "Raw Data"], index=0)
+
+# Conditionally display the analysis type radio button
+if dataset_choice == "Raw Data":
+    analysis_type = st.sidebar.radio("Select analysis type:", ["Summary", "Raw Data Overview"])
+
+    if analysis_type == "Summary":
+        # Plot the distribution using a bar plot
+        # Check the distribution of sentiment categories
+        # Display summary numbers
+        total_brands = df2['brand'].nunique()
+        total_reviews = df2['reviewText'].count()
+        st.write(f"**Summary Information**")
+        st.write(f"Total Number of Brands: {total_brands}")
+        st.write(f"Total Number of Reviews: {total_reviews}")
+
+        # Plotting the overall rating distribution
+        st.subheader("Overall Rating Distribution")
+        plt.figure(figsize=(8, 6))
+        df2['overall'].value_counts().sort_index().plot(kind='bar', color='blue')
+        plt.title('Distribution of Overall Ratings')
+        plt.xlabel('Ratings')
+        plt.ylabel('Count')
+        plt.xticks(rotation=0)
+        plt.tight_layout()
+        st.pyplot(plt)  # Display the plot
+
+        # Plot the sentiment distribution using a bar plot
+        st.subheader("Sentiment Distribution Based on Ratings")
+        sentiment_counts = df2['sentiment'].value_counts()
+        plt.figure(figsize=(8, 6))
+        sentiment_counts.plot(kind='bar', color=['green', 'yellow', 'red'])
+        plt.title('Sentiment Distribution based on Ratings')
+        plt.xlabel('Sentiment')
+        plt.ylabel('Count')
+        plt.xticks(rotation=0)
+        plt.tight_layout()
+        st.pyplot(plt)  # Display the plot
 
 
-dataset_choice = st.sidebar.radio("Know your Customers", ["Sentiment Analysis", "Customer Feedback and Reviews"])
+if dataset_choice == "Processed Data":
+    analysis_type = st.sidebar.radio("Know your Customers", ["Sentiment Analysis", "Customer Feedback and Reviews"])
 
+    if analysis_type == "Sentiment Analysis":
+        # Positive reviews word cloud
+        positive_reviews = df2[df2['sentiment_reviewText'] == 'positive']['reviewText']
+        if not positive_reviews.empty:
+            st.subheader("Positive Reviews Word Cloud")  # Adding a header for positive reviews
+            wordcloud_text_positive = ' '.join(positive_reviews)
+            wordcloud_positive = WordCloud(stopwords=STOPWORDS, background_color='white').generate(wordcloud_text_positive)
+            fig_positive, ax_positive = plt.subplots()
+            plt.imshow(wordcloud_positive, interpolation='bilinear')
+            plt.axis("off")
+            st.pyplot(fig_positive)
+        else:
+            st.subheader("Positive Reviews Word Cloud")  # Header displayed even if no reviews are available
+            st.write("No positive reviews available for generating a word cloud.")
 
-if dataset_choice == "Sentiment Analysis":
-    st.header("Sample Data")
-    st.dataframe(df1.head(20))  
+        # Negative reviews word cloud
+        negative_reviews = df2[df2['sentiment_reviewText'] == 'negative']['reviewText']
+        if not negative_reviews.empty:
+            st.subheader("Negative Reviews Word Cloud")  # Adding a header for negative reviews
+            wordcloud_text_negative = ' '.join(negative_reviews)
+            wordcloud_negative = WordCloud(stopwords=STOPWORDS, background_color='black', contour_color='white').generate(wordcloud_text_negative)
+            fig_negative, ax_negative = plt.subplots()
+            plt.imshow(wordcloud_negative, interpolation='bilinear')
+            plt.axis("off")
+            st.pyplot(fig_negative)
+        else:
+            st.subheader("Negative Reviews Word Cloud")  # Header displayed even if no reviews are available
+            st.write("No negative reviews available for generating a word cloud.")
 
-elif dataset_choice == "Customer Feedback and Reviews":
-    st.header("Sample Data")
-    st.dataframe(df2.head(20)) 
-
-
-col1, col2 = st.columns(2)
-
-# Plotting in the first column
-with col1:
-    st.header("Overall Rating Distribution")
-    fig1, ax1 = plt.subplots()
-    df1['overall'].plot(kind='hist', bins=20, title='Distribution of Overall Ratings - ' + dataset_choice)
-    ax1.spines[['top', 'right']].set_visible(False)
-    st.pyplot(fig1)
+    if analysis_type == "Customer Feedback and Reviews":
+        st.subheader("Distribution of Sentiment Scores")
+        # Plotting histogram of sentiment scores from review text
+        plt.figure(figsize=(10, 4))
+        plt.hist(df2['sentiment_score_reviewText'], bins=20, color='purple', alpha=0.7)
+        plt.title("Histogram of Sentiment Scores from Review Text")
+        plt.xlabel("Sentiment Score")
+        plt.ylabel("Frequency")
+        st.pyplot(plt)
     
-
-
-logger = logging.getLogger(__name__)
-logger.info("Application started successfully")
-
+        # Aggregate the average sentiment scores per brand and display top 10 and bottom 10 brands
+        brand_sentiment_avg = df2.groupby('brand')['sentiment_score_reviewText'].mean().sort_values()
+    
+        st.subheader("Top 10 Brands with Positive Reviews")
+        top_10_brands = brand_sentiment_avg.tail(10)
+        plt.figure(figsize=(10, 4))
+        top_10_brands.plot(kind='barh', color='green')
+        plt.title("Top 10 Brands by Average Sentiment Score")
+        plt.xlabel("Average Sentiment Score")
+        st.pyplot(plt)
+    
+        st.subheader("Bottom 10 Brands with Negative Reviews")
+        bottom_10_brands = brand_sentiment_avg.head(10)
+        plt.figure(figsize=(10, 4))
+        bottom_10_brands.plot(kind='barh', color='red')
+        plt.title("Bottom 10 Brands by Average Sentiment Score")
+        plt.xlabel("Average Sentiment Score")
+        st.pyplot(plt)
+    
+        # Comparison of sentiment counts based on rating vs review text
+        st.subheader("Comparison of Sentiment Counts: Rating vs Review Text")
+        comparison_df = pd.DataFrame({
+            'Sentiment from Ratings': df2['sentiment'].value_counts(),
+            'Sentiment from Review Text': df2['sentiment_reviewText'].value_counts()
+        })
+    
+        plt.figure(figsize=(10, 5))
+        comparison_df.plot(kind='bar', color=['skyblue', 'orange'])
+        plt.title("Sentiment Comparison: Ratings vs Review Text")
+        plt.xlabel("Sentiment Type")
+        plt.ylabel("Counts")
+        plt.xticks(rotation=0)
+        st.pyplot(plt)
